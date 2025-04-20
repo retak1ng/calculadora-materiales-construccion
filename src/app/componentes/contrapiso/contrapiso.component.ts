bb2c3bc2141cf22b020ea602ef8cb1092d2cd3c9
Martín Rojas <csamuelr.lp@gmail.com>
2025-04-20 20:15:50 -0300

    Primer Commit

---
+++ src/app/componentes/contrapiso/contrapiso.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contrapiso',
+  imports: [FormsModule],
+  templateUrl: './contrapiso.component.html',
+  styleUrl: './contrapiso.component.css'
+})
+export class ContrapisoComponent {
+  matUsandoCalYCemento = [
+    { nombre: '1 Cal', cantidad: 81.00, unidad: 'kg', input: 0, medComercial: 25 },
+    { nombre: '1/4 Cemento', cantidad: 38.40, unidad: 'kg', input: 0, medComercial: 50 },
+    { nombre: '4 Arena', cantidad: 0.515, unidad: 'm3', input: 0, medComercial: 1 },
+    { nombre: '6 Cascote', cantidad: 0.770, unidad: 'm3', input: 0, medComercial: 1 },
+  ]
+
+  matUsandoCemento = [
+    { nombre: '1 Cemento', cantidad: 108.00, unidad: 'kg', input: 0, medComercial: 50 },
+    { nombre: '4 Arena', cantidad: 0.430, unidad: 'm3', input: 0, medComercial: 1 },
+    { nombre: '8 Cascote', cantidad: 0.900, unidad: 'm3', input: 0, medComercial: 1 },
+  ]
+}
---
+++ src/app/main/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ContrapisoComponent } from "../../componentes/contrapiso/contrapiso.component";
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [ContrapisoComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+
+}
